17e4242e44b7d0781fb3a2f1c096482fa825a13d
Hummel009 <hummel.turbamentum@gmail.com>
2025-11-17 19:29:06 +0300

    Update build.gradle.kts

--- appForgeArchive/1.7.10/build.gradle.kts
+++ appForgeArchive/1.7.10/build.gradle.kts
@@ -2,7 +2,7 @@ import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 plugins {
-	id("com.gtnewhorizons.retrofuturagradle") version "latest.release"
+	id("com.gtnewhorizons.retrofuturagradle") version "1.4.9"
 }
 
 group = "com.github.hummel"
